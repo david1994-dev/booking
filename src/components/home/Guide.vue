174b48b49b6938204ad32b28b2ae467ff93796dc
Chien Nguyen <chiennd@peacesoft.net>
2018-01-15 14:53:44 +0700

    new index

---
+++ src/components/home/Guide.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="guide-book tp-box">
+  <div class="tp-wrap-title">
+    <h3 class="tp-title"><strong>{{ $t('home.steps.to_be_beautiful') }}</strong> {{ $t('home.steps.in_4_steps') }}</h3>
+  </div>
+  <div class="ofhd">
+    <div class="list">
+      <div class="item">
+        <div class="name">{{ $t('home.steps.step1') }}</div>
+        <div class="content-item">
+          <div class="number">1</div>
+          <p class="des">{{ $t('home.steps.step1_detail') }}</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="name">{{ $t('home.steps.step2') }}</div>
+        <div class="content-item">
+          <div class="number">2</div>
+          <p class="des">{{ $t('home.steps.step2_detail') }}</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="name">{{ $t('home.steps.step3') }}</div>
+        <div class="content-item">
+          <div class="number">3</div>
+          <p class="des">{{ $t('home.steps.step3_detail') }}</p>
+        </div>
+      </div>
+      <div class="item">
+        <div class="name">{{ $t('home.steps.step4') }}</div>
+        <div class="content-item">
+          <div class="number">4</div>
+          <p class="des">{{ $t('home.steps.step4_detail') }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'Guide'
+}
+</script>
