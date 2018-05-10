da36a88712aac89f775698e865df8bfc6d932bb7
ManhTuan9186 <tuannm01786@gmail.com>
2018-05-10 16:32:53 +0700

    Demo server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://dev.bzone.vn/api/v1"',
+  API_URL: '"http://bzone-server.tk/api/v2"',
   FB_APP_ID: '""',
   GOOGLE_MAP_KEY: '""',
   DOMAIN: '"http://www.bzone.cf/"',
