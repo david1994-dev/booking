6d7d19018137ab8839cba15e48f9d5eac3a72b55
ducsatthu <ductranxuan.29710@gmail.com>
2018-05-21 14:16:44 +0700

    Make example prop token

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"http://bzone-server.tk/api/v2"',
-  FB_APP_ID: '""',
-  GOOGLE_MAP_KEY: '""',
+  FB_APP_ID: '"1637966656508513"',
+  GOOGLE_MAP_KEY: '"AIzaSyC4Lr23cLmL1FNhKbVZepiG6MsXcwNPvVA"',
   DOMAIN: '"http://bzone-server.tk/"',
-  MIXPANEL_PROJECT_TOKEN: '""'
+  MIXPANEL_PROJECT_TOKEN: '"3520d09f4365e0cf75b2f1ecde1c5f46"'
 }
