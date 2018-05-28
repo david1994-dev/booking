06570f533bee422b218776187f4350b4cba7d8e4
root <root@ip-172-30-0-120.ap-southeast-1.compute.internal>
2018-05-28 08:18:44 +0000

    Update propd

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://bzone-server.tk/api/v2"',
+  API_URL: 'http://dev.booking.bzone.vn/api/v2"',
   FB_APP_ID: '"1637966656508513"',
   GOOGLE_MAP_KEY: '"AIzaSyC4Lr23cLmL1FNhKbVZepiG6MsXcwNPvVA"',
-  DOMAIN: '"http://bzone-server.tk/"',
+  DOMAIN: '"http://dev.booking.bzone.vn"',
   MIXPANEL_PROJECT_TOKEN: '"3520d09f4365e0cf75b2f1ecde1c5f46"'
 }
